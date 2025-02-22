871302a5db112e5caf5a0ab8a5b7659cc8f32048
Davi Bispo <davibispo@localhost.localdomain>
2025-02-22 19:03:13 -0300

    initial commit

---
+++ src/main/java/com/davib/challenger_magalu/ChallengerMagaluApplication.java
@@ -0,0 +1,13 @@
+package com.davib.challenger_magalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerMagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davib/challenger_magalu/ChallengerMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davib.challenger_magalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
