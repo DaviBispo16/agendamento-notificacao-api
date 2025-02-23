28050e38f5875f53be1cfe5d5d03745eaa660be3
Davi Bispo <davibispo@localhost.localdomain>
2025-02-22 21:35:07 -0300

    feat(entities): added search notifications methods

--- src/main/java/com/davib/challenger_magalu/business/SchedulingService.java
+++ src/main/java/com/davib/challenger_magalu/business/SchedulingService.java
@@ -3,6 +3,7 @@ package com.davib.challenger_magalu.business;
 import com.davib.challenger_magalu.business.mapper.ISchedulingMapper;
 import com.davib.challenger_magalu.controller.dto.in.SchedulingRecord;
 import com.davib.challenger_magalu.controller.dto.out.SchedulingRecordOut;
+import com.davib.challenger_magalu.infrastructure.exception.NotFoundException;
 import com.davib.challenger_magalu.infrastructure.repository.SchedulingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,11 @@ public class SchedulingService {
     public SchedulingRecordOut recordScheduling(SchedulingRecord scheduling){
         return schedulingMapper.forOut(
                 repository.save(schedulingMapper.forEntity(scheduling)));
+    }
+
+    public SchedulingRecordOut findSchedulingById(Long id) {
+        return schedulingMapper.forOut(repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("ID não encontrado")));
     }
 
 
--- src/main/java/com/davib/challenger_magalu/controller/SchedulingController.java
+++ src/main/java/com/davib/challenger_magalu/controller/SchedulingController.java
@@ -1,20 +1,25 @@
-package com.davib.challenger_magalu.controller.dto;
+package com.davib.challenger_magalu.controller;
 
 import com.davib.challenger_magalu.business.SchedulingService;
 import com.davib.challenger_magalu.controller.dto.in.SchedulingRecord;
 import com.davib.challenger_magalu.controller.dto.out.SchedulingRecordOut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/agendamento")
 public class SchedulingController {
     private SchedulingService schedulingService;
+
+    @PostMapping
     public ResponseEntity<SchedulingRecordOut> recordScheduling(@RequestBody SchedulingRecord scheduling) {
         return ResponseEntity.ok(schedulingService.recordScheduling(scheduling));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SchedulingRecordOut> findSchedulingById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(schedulingService.findSchedulingById(id));
     }
 }
